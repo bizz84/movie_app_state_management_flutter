33ea4764e21e2f9166ee53005dc09eb696d4b466
Andrea Bizzotto <codewithandrea@gmail.com>
2021-12-16 22:41:52 +0000

    Add comment

--- apps/get_it/lib/app/app_startup/app_startup_model.dart
+++ apps/get_it/lib/app/app_startup/app_startup_model.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
+import 'package:core/models/app_models/profiles_data.dart';
 import 'package:core/models/app_state/app_startup_state.dart';
 import 'package:core/persistence/data_store.dart';
 import 'package:flutter/foundation.dart';
+import 'package:movie_app_demo_flutter/service_locator.dart';
 
 class AppStartupModel extends ValueNotifier<AppStartupState> {
   AppStartupModel({required this.dataStore})
@@ -22,10 +24,15 @@ class AppStartupModel extends ValueNotifier<AppStartupState> {
     _subscription = dataStore.profilesData().listen((profilesData) {
       if (profilesData.selectedId == null || profilesData.profiles.isEmpty) {
         value = AppStartupState.needsProfile();
-        // TODO: Should we register profilesData as a singleton?
       } else {
         value = AppStartupState.profileLoaded(profilesData);
       }
+      // TODO: Is this the correct way of registering values that can change?
+      // if (getIt.isRegistered<ProfilesData>()) {
+      //   getIt.resetLazySingleton<ProfilesData>(instance: profilesData);
+      // } else {
+      //   getIt.registerSingleton<ProfilesData>(profilesData);
+      // }
     });
   }
 }
